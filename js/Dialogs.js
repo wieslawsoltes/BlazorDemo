78c14811b73981111ee3d115aa10cb2cf4cffeff
wieslawsoltes <wieslawsoltes@users.noreply.github.com>
2022-02-17 11:30:58 +0000

    Deploying to gh-pages from @ wieslawsoltes/BlazorDemo@df3347c5acc6b7b4d63bde4d86795549fb24a297 🚀

--- js/Dialogs.js
+++ js/Dialogs.js
@@ -1,17 +1,10 @@
-//
-// Open File
-//
 
-async function ShowOpenFileDialog(inputElement, filter, multiple, openFolder) {
+async function triggerInputElement(inputElement, filter, multiple, openFolder) {
   inputElement.accept = filter;
   inputElement.multiple = multiple;
   inputElement.webkitdirectory = openFolder;
   await inputElement.click();
 }
-
-//
-// Save File
-//
 
 async function downloadFileFromStream(fileName, contentStreamReference) {
   const arrayBuffer = await contentStreamReference.arrayBuffer();
@@ -29,73 +22,3 @@ function triggerFileDownload(fileName, url) {
   anchorElement.click();
   anchorElement.remove();
 }
-
-//
-// File Pickers
-//
-
-// https://wicg.github.io/file-system-access/
-// https://web.dev/file-system-access/
-// https://w3c.github.io/FileAPI/#dfn-file
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/showOpenFilePicker
-
-function GetPropertyValue(obj, property) {
-  return obj[property];
-}
-
-function SetPropertyValue(obj, property, value) {
-  return obj[property] = value;
-}
-
-async function ShowOpenFilePicker() {
-
-  const opts = {
-    types: [
-      {
-        description: 'Text file',
-        accept: {'text/plain': ['.txt']},
-      },
-    ],
-    excludeAcceptAllOption: true,
-    multiple: false
-  };
-
-  const fileHandles = await window.showOpenFilePicker(opts);
-  const fileHandle = fileHandles[0];
-  return fileHandle;
-  // return fileHandle.Name;
-  // const file = await fileHandles[0].getFile();
-  // return file;
-}
-
-// USAGE:
-// var fileHandle = await ShowOpenFilePicker();
-// var file = await fileHandle.getFile();
-// var stream = await file.stream();
-// var reader = await stream.getReader();
-// var text = await file.text();
-// var arrayBuffer = await file.arrayBuffer();
-
-// https://developer.mozilla.org/en-US/docs/Web/API/window/showSaveFilePicker
-
-function ShowSaveFilePicker() {
-    const opts = {
-      types: [{
-        description: 'Text file',
-        accept: {'text/plain': ['.txt']},
-      }],
-    };
-    const fileHandle = window.showSaveFilePicker(opts);
-    return fileHandle;
-  }
-
-// https://developer.mozilla.org/en-US/docs/Web/API/window/showDirectoryPicker
-
-async function ShowDirectoryPicker() {
-  const dirHandle = await window.showDirectoryPicker();
-  return dirHandle;
-}
-
-
-// const dirHandle = await window.showDirectoryPicker();
-// var entries = await dirHandle.entries();
